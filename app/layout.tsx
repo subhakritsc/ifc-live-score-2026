80172bd97cf28e7b8ce11373bf5fff22975851d7
subhakritsc <keenchokchaitam@gmail.com>
2026-03-23 00:46:19 +0700

    feat: use logo.png as favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Google+Sans:wght@400;500;700&display=swap" rel="stylesheet" />
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>⚽</text></svg>" />
+        <link rel="icon" href="/logo.png" />
       </head>
       <body className={`${notoSansThai.variable} font-sans antialiased bg-background text-foreground`}>
         {children}
